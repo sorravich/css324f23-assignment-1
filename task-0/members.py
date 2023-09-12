9cf60ecb25a0e7489571141939f7d5be1a65f7a0
sorravich <144808033+sorravich@users.noreply.github.com>
2023-09-12 23:51:11 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Sorravich Rattagool", "sid": "6422782183"},
+    {"name": "Sirapich Boonyasampan", "sid": "6422782936"},
 ]
